019d49fd8cb3461af92e50f3dede7800a557a373
TaVitka <tavitka17@gmail.com>
2024-05-28 04:39:02 +0300

    added featured slider section for homepage + adaptive

---
+++ src/js/components/_slider.js
@@ -0,0 +1,28 @@
+import Swiper, { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper';
+
+let options = {
+  speed: 1400,
+  autoplay: {
+    delay: 6000,
+  },
+  modules: [Navigation, Scrollbar, Pagination, Autoplay],
+  draggable: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.slider__arrow--next',
+    prevEl: '.slider__arrow--prev',
+  },
+  breakpoints: {
+    476: {
+      slidesPerView: 'auto',
+      spaceBetween: 24,
+    },
+
+    1: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    }
+  }
+};
+
+const swiperFeatured = new Swiper('.slider__featured', options);
