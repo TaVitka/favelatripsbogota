c0784cdc8ace1cf2f178480807359d73c05fa79d
TaVitka <tavitka17@gmail.com>
2024-05-28 18:26:39 +0300

    added favela and tourism pages + adaptive

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -27,3 +27,19 @@ let options = {
 
 const swiperFeatured = new Swiper('.slider__featured', options);
 const swiperTeam = new Swiper('.slider__team', options);
+
+const swiperPlaces= new Swiper('.hero__slider', {
+  slidesPerView: 1,
+  spaceBetween: 24,
+  speed: 1400,
+  autoplay: {
+    delay: 6000,
+  },
+  modules: [Navigation, Scrollbar, Pagination, Autoplay],
+  draggable: true,
+  grabCursor: true,
+  navigation: {
+    nextEl: '.slider__arrow--next',
+    prevEl: '.slider__arrow--prev',
+  },
+});
